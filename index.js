4a4d807c2315805b79ad564637dfd9fde402c85f
becuz <albertopierini@gmail.com>
2017-11-27 14:16:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+/**
+ * Simple memory cache implementation.
+ * 
+ */
+class Funes {
+
+  constructor(conf) {
+      if (!conf) conf = {};
+
+      this.size    = conf.size || 10000;
+      this.validity = conf.validity == 0 ? conf.validity : conf.validity || 5 * 60 * 1000;
+      this.responseField = conf.responseField || 'body';
+      this.idField = conf.idField || 'id';
+      this.retrieve = conf.retrieve;
+
+      this.cache = {
+          queue: [],
+          objects: {}   //objects: {id: {object:{id:1, ..}, lastAccess:"2017-11-27T10:43:10.264Z"}}
+      }
+  }
+
+  /**
+  * Gets an element (or more) from the cache and, if it's not present, retrieves it and puts it into the cache (or returns null)
+  *
+  */
+  get(ids){
+      var requestIsArray = Array.isArray(ids);
+      if (!Array.isArray(ids)) ids = [ids];
+
+      var now = new Date().getTime();
+
+      return new Promise((resolve, reject) => {
+          var result = [];
+          var idsNotInCache = [];
+          ids.forEach(id => {
+                if (this.cache.objects[id]
+                        && (now - this.cache.objects[id].lastAccess < this.validity)){
+                    var object = this.cache.objects[id].object;
+                    result.push(object);
+                    this.put(object)
+                } else {
+                    idsNotInCache.push(id);
+                }
+          });
+
+          var promise = (idsNotInCache.length > 0) && this.retrieve
+            ?
+            this._retrieve(idsNotInCache)
+              .then(response => {
+                  response[this.responseField].forEach(object => {
+                      result.push(object);
+                      this.put(object)
+                  })
+              })
+            :
+            Promise.resolve(null);
+
+          promise
+          .then( () => {return resolve(requestIsArray ? result : result[0] || null);})
+          .catch(reject)
+      })
+
+  }
+
+  /**
+  * Put an element in the cache
+  */
+  put(objects){
+      if (!Array.isArray(objects)) objects = [objects];
+
+      objects.forEach(object => {
+
+          this.cache.objects[object[this.idField]] = {
+              object: object,
+              lastAccess: new Date()
+          }
+
+          var oldIndex = this.cache.queue.indexOf(object[this.idField]);
+          this.cache.queue.splice(0, 0, object[this.idField]);
+          if (oldIndex > -1){
+              this.cache.queue.splice(oldIndex+1, 1);
+          } else if (this.cache.queue.length > this.size){
+              var idToDelete = this.cache.queue[this.cache.queue.length-1];
+              this.cache.queue.splice(this.cache.queue.length-1, 1);
+              delete this.cache.objects[idToDelete];
+          }
+
+      })
+  }
+
+  /**
+  *
+  */
+  _retrieve(ids){
+      return this.retrieve(ids);
+  }
+
+}
+
+module.exports = Funes;
---
+++ test/funes.js
@@ -0,0 +1,133 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Funes = require('../index');
+
+describe('funes.js', () => {
+    
+    it('get null should return null when empty', done => {
+        new Funes().get(null).then(obj => {
+            expect(obj).to.be.null;
+            done();
+        });
+    });
+
+    it('get null should return [] when empty', done => {
+        new Funes().get([null]).then(obj => {
+            expect(obj).to.be.an('array').to.have.lengthOf(0);
+            done();
+        });
+    });
+
+    it('get 1 should return null when empty', done => {
+        new Funes().get(1).then(obj => {
+            expect(obj).to.be.null;
+            done();
+        });
+    });
+
+    it('get [1] should return [] when empty', done => {
+        new Funes().get([1]).then(obj => {
+            expect(obj).to.be.an('array').to.have.lengthOf(0);
+            done();
+        });
+    });
+
+    it('get should return same obj', done => {
+        var defaultCache = new Funes();
+        var obj = {id: 1, name: 'a'};
+        defaultCache.put(obj);
+        defaultCache.get(1).then(obj => {
+            expect(obj).to.be.equal(obj);
+            done();
+        });
+    });
+
+    it('should be fifo', done => {
+        var cache = new Funes({size: 1});
+        var object = {id: 1, name: 'a'};
+        var object2 = {id: 2, name: 'b'};
+        cache.put([object, object2]);
+        cache.get([1, 2]).then(obj => {
+            expect(obj).to.be.an('array').to.have.lengthOf(1);
+            expect(obj[0]).to.be.equals(object2);
+            done();
+        });
+    });
+
+
+    it('queue should works fine', done => {
+        var cache = new Funes({size: 5});
+        var ids = [];
+        for(var i=0;i<10;i++){
+            cache.put({id: i});
+            ids.push(i);
+        }
+
+        cache.get(ids).then(obj => {
+            expect(obj).to.be.an('array').to.have.lengthOf(5);
+
+            cache.get(2).then(obj => {
+                expect(obj).to.be.null
+
+                cache.get(8).then(obj => {
+                    expect(obj.id).to.be.equal(8);
+
+                    expect(cache.cache.queue).to.be.an('array').to.have.lengthOf(5);
+                    expect(cache.cache.queue[0]).to.be.equal(8);
+                    expect(cache.cache.queue[1]).to.be.equal(9);
+                    expect(cache.cache.queue[2]).to.be.equal(7);
+                    expect(cache.cache.queue[3]).to.be.equal(6);
+                    expect(cache.cache.queue[4]).to.be.equal(5);
+                    done();
+                })
+            })
+        });
+    });
+
+    it('should ignore old objects', done => {
+        var cache = new Funes({validity: 10});
+        var object = {id: 1, name: 'a'};
+        var object2 = {id: 2, name: 'b'};
+        cache.put([object, object2]);
+        setTimeout(function(){
+            cache.get([1, 2]).then(obj => {
+                expect(obj).to.be.an('array').to.have.lengthOf(0);
+                done();
+            });
+        }, 11)
+    });
+
+    it('should overwrite previous objects', done => {
+        var cache = new Funes();
+        cache.put({id: 1, name: 'a'});
+        cache.put({id: 1, name: 'b'});
+        cache.get(1).then(obj => {
+            expect(obj.name).to.be.equal('b');
+            done();
+        });
+    });
+
+    it('should retrieve new objects', done => {
+        var cache = new Funes(
+            {
+                validity: 0,
+                retrieve: function(ids){
+                    expect(ids).to.be.an('array').to.have.lengthOf(1);
+                    expect(ids[0]).to.be.equal(1);
+                    return Promise.resolve({body: [{id:1, name: 'a'}]})
+                }
+            });
+
+        cache.put({id:1, name: 'b'});
+        setTimeout(
+                function(){
+                    cache.get(1).then(obj => {
+                        expect(obj.id).to.be.equal(1);
+                        expect(obj.name).to.be.equal('a');
+                        done();
+                    });
+                }, 3
+        );
+    });
+
+});
